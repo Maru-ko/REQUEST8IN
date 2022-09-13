a0b9bc96b74f56fcc8a10bc660b72f654fbf5817
Marcos <53981643+Maru-ko@users.noreply.github.com>
2022-09-13 15:01:09 -0400

    hello

--- Marcos/REQUEST8IN/index.js
+++ Marcos/REQUEST8IN/index.js
@@ -2,5 +2,9 @@
 import Express from "express";
 
 const app = Express();
+const port = 3001;
+app.get("/", (req, res) => {
+  res.send("nGrok4Life");
+})
 
-//
+app.listen(port, () => console.log("we are listening on por" + port))
