864a0e5b471f8083f561cfd9fb6bf5380835fb89
Marcos <53981643+Maru-ko@users.noreply.github.com>
2022-09-13 18:34:20 -0400

    Clean Up

--- App.js
+++ App.js
@@ -1,10 +1,3 @@
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
-
-// import Express from "express";
 const express = require('express')
 const app = express();
 app.set('view engine', 'pug')
@@ -12,16 +5,11 @@ app.use(express.static('static'))
 
 const PORT = process.env.PORT || 3000;
 
-// app.get('^/$|index(.html)?', (req, res) => {
 app.get('/', (req, res) => {
 
   res.render('home')
 
 });
-
-// app.get('/*', (req, res) => {
-
-// })
 
 
 
