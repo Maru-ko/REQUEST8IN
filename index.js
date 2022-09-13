f8981a0e1d108cc07ad46daa976a3d581456c10b
Marcos <53981643+Maru-ko@users.noreply.github.com>
2022-09-13 17:46:31 -0400

    idk

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-
+// const express = require('express');
 import Express from "express";
+const app = Express();
+  // const path = require('path')
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  // res.send('hey')
+  // res.sendFile('./views/')
+});
+
+// import Express from "express";
 // import { compareAsc, format } from 'date-fns'
 // import Stuff from "stuff.js"
 // const { v4: uuid } = require('uuid')
 // format(new Date(2014, 1, 11), 'yyyy-MM-dd')
 // const eventEmitter = require('events')
 
-const app = Express();
-const port = 3001;
-app.get("/", (req, res) => {
-  res.send("nGrok4Life");
-  // res.json(Stuff)
-})
+// const app = Express();
+// const port = 3001;
+// app.get("/", (req, res) => {
+//   res.send("nGrok4Life");
+//   // res.json(Stuff)
+// })
 
 
 
-app.listen(port, () => console.log("we are listening on por" + port))
+app.listen(PORT, () => console.log("we are listening on por" + PORT))
