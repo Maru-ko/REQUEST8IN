c2047cbccb9bf47052defa2a959740b1a0dde147
Marcos <53981643+Maru-ko@users.noreply.github.com>
2022-09-13 21:57:13 -0400

    will get back to it after I eat...

--- App.js
+++ App.js
@@ -1,43 +1,34 @@
 const express = require('express')
-// const router = express.Router();
-// const { v4: uuid } = require('uuid')
+
+const { v4: uuid } = require('uuid')
 const app = express();
+const flash = require("express-flash")
 app.set('view engine', 'pug')
 app.use(express.static('static'))
 
-// app.use(express.urlencoded({
-//   extended: true
-// }))
 const PORT = process.env.PORT || 3003;
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home'); 
 });
 
-app.get('/bin/:bin_id', (req, res) => {
-  // res.render('bin')
+app.get('/bin/:bin_id', (req, res) => { // 
+
 });
 
 app.get('/bin/:bin_id/view', (req, res) => {
-  // res.render
+  res.render('bin')
 });
 
-app.post('/', (req, res) => {
-  const user = new User({
-      username: req.body.username,
-      password: req.body.password
-  });
-
-  user.save()
-  .then(data => {
-      res.json(data);
-  })
-  .catch(err => {
-      res.json({message: err})
-  })
+app.post('"/bin/:bin_id"', (req, res) => { 
+  let bin = 'codetomakeabinGoeshere'
+  // check if ID is in DB
+  flash("OK!", "No errors I guess.");
+  res.redirect(302, `/bin/${bin.id}/view`)
 })
 
-app.post('/bin/:bin_id', (req, res) => {
 
-});
+// app.post('/', (req, res) => {
+// });
+
 app.listen(PORT, () => console.log("we are listening on port" + PORT))
