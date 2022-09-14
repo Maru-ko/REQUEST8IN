5fde7e0381e9991c7708f1cb351b8f5cc20bff83
Marcos <53981643+Maru-ko@users.noreply.github.com>
2022-09-14 17:47:48 -0400

    Just updating 'App.js'

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
-const express = require('express')
-// const database = require('wherever the database is')
-
+const express = require('express');
+const database = require("./data/configs/Data");
 // const flash = require("express-flash")
-  const { v4: uuid } = require('uuid')
+const { v4: uuid } = require('uuid');
+// const { addRequest, createBin } = require('./data/configs/Data');
 const app = express();
 
 app.set('view engine', 'pug')
 app.use(express.static('static'))
-app.use(express.json())
 
 
 const PORT = 3003;
@@ -17,39 +16,48 @@ app.get('/', (req, res) => {
   res.status(200).render('home'); 
 });
 
-app.get('/bin/:bin_id', (req, res) => { // 
-  const bin = `search 'database' for ${req.params.binID}`;
-  if (bin) {
-    res.status(200).render(`bin/${bin.id}`)
+app.get('/bin/:bin_id/view', async (req, res) => {
+  let bin_id = req.params.bin_id;
+  const binExists = await database.binExists(bin_id);
+  if (binExists) {
+    const reqs = await database.getRequests(bin_id);
+    res.render('bin', {
+      binid:bin_id,
+      reqs
+    });
   } else {
-    res.sendStatus(404).render('404') // or just show the appropriate bin
+    res.sendStatus(301).redirect('/');
   }
 });
 
-app.get('/bin/:bin_id/view', (req, res) => {
-  // No clue
-  //res.render('bin')
+//adds a request to a bin if the bin exists
+app.get('/bin/:bin_id', async (req, res) => { // 
+ const binExists = await database.binExists(req.params.bin_id);
+
+  if (binExists) {
+    await database.addRequest(req.params.bin_id, req) // we may need to add a error handling || if the db update does not succeed
+    res.sendStatus(201);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
-app.post('"/bin/:bin_id"', (req, res) => { 
+//adding post requests to a bin
+app.post('"/bin/:bin_id"', async (req, res) => { 
+  const binExists = await database.binExists(req.params.bin_id);
 
-  let request;
-      // check if req.ID is not in bin/DB
-  if ('request not in bin') {
-    request = 'code to add request to bin goes here'
-    // flash("OK!", "No errors I guess.");
-    res.status(301).redirect(`/bin/${req.id}/view`)
+  if (binExists) {
+    await database.addRequest(req.params.bin_id, req)
+    res.sendStatus(201);
   } else {
-    // display error 
-    res.status(400).render('home');
+    res.sendStatus(404);
   }
 })
 
-app.post('/', (req, res) => {
-  //replace with call to database module to create a bin
-  let newBinId = Math.floor(Math.random()*1000000)
-
-  res.json({newBinId})
+//create a new bin and return its newbinID as json
+app.post('/', async (req, res) => {
+  let newBin = await database.createBin()
+  res.json({ newBinId })
 });
 
 app.listen(PORT, () => console.log("we are listening on port" + PORT))
