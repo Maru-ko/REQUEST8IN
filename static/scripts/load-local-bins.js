2313cf02b9f5e0d2fa35e8f30ae0ede9f69fa756
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-09-15 17:03:41 -0500

    added /verifybins route to verify local storage

--- static/scripts/load-local-bins.js
+++ static/scripts/load-local-bins.js
@@ -1,7 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   let existing = localStorage.getItem("bins");
+  
   if (existing) {
-    const bin_ids = JSON.parse(existing);
+    let bin_ids = JSON.parse(existing);
+    let res = await fetch(`https://${location.hostname}/verifybins`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(bin_ids)
+    })
+
+    bin_ids = await res.json(); 
+    localStorage.setItem("bins", JSON.stringify(bin_ids));
     const container = document.querySelector('#sidebar-items')
     bin_ids.forEach(id => {
       const link = document.createElement('a');
