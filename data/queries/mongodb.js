e3014fd210e3926429946d0e8728e45b5ff5611b
Cody Williams <61119166+Code-yWilliams@users.noreply.github.com>
2022-09-14 18:51:59 -0700

    reversing requests from db so newest come first:

--- data/queries/mongodb.js
+++ data/queries/mongodb.js
@@ -32,7 +32,7 @@ const getRequestsByBinId = async (binId) => {
   const bins = await BinModel.find({ binId: binId });
   const bin = bins[0];
   const requests = bin.requests
-  return requests;
+  return requests.reverse();
 }
 
 const mongoQueries = {
